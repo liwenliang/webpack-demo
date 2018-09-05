fbbae947707d021274b6ed5c34845f548d358b03
liwenliang <liwenliang@yixia.com>
2018-09-05 20:05:57 +0800

    懒加载：示例

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-function getComponent () {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-    var element = document.createElement('div')
-    var _ = _.default
+import _ from 'lodash'
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+function component () {
+  var element = document.createElement('div')
+  var button = document.createElement('button')
+  var br = document.createElement('br')
 
-    return element
+  button.innerHTML = 'Click me and look at the console!'
+  element.innerHTML = _.join(['Hello', 'Webpack'], ' ')
 
-  }).catch(error => 'An error occurred while loading the component')
+  element.appendChild(br)
+  element.appendChild(button)
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default
+    print()
+  })
+
+  return element
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component)
-})
+document.body.appendChild(component())
