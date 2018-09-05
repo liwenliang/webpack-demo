e614679c4f7ceeb4cb47d7089f7164981b9d056a
liwenliang <liwenliang@yixia.com>
2018-09-05 18:30:52 +0800

    Tree Shaking：Add a Utility

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import _ from 'lodash'
-import printMe from './print.js'
-import './styles.css'
+import { cube } from './math.js'
 
 function component () {
-  let element = document.createElement('div')
-  let btn = document.createElement('button')
+  let element = document.createElement('pre')
 
-  element.innerHTML = _.join(['Heldlo', 'webpack'], ' ')
-
-  btn.innerHTML = 'Click me and check the console!'
-  btn.onclick = printMe
-
-  element.appendChild(btn)
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cube is equal to ' + cube(5)
+  ].join('\n\n')
 
   return element
 }
