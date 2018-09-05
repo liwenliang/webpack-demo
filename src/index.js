6aba4360e11e2d002f0727c47ead5b73ae80608a
liwenliang <liwenliang@yixia.com>
2018-09-05 19:50:50 +0800

    代码分离：入口起点(entry points) & 防止重复(prevent duplication)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import { cube } from './math.js'
-
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!')
-}
+import _ from 'lodash';
 
 function component () {
   let element = document.createElement('pre')
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cube is equal to ' + cube(5)
-  ].join('\n\n')
+  element.innerHTML = _.join(['Hello', 'Webpack'], ' ')
 
   return element
 }
